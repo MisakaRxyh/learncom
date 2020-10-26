fe5699ad115bb4ac8f8ba80d12fc196bf040d2e6
MisakaRxyh <825559915@qq.com>
2020-10-26 16:19:16 +0800

    init repo

---
+++ src/test/java/cn/water/learncom/LearncomApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.water.learncom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearncomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
